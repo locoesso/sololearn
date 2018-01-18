531e27e1527ebb5e4f47b5cf006d7b31e558f5fa
Esa Hanikka <esa.hanikka@gmail.com>
2018-01-18 19:37:28 +0200

    11110000

--- csharp/ifelse/Program.cs
+++ csharp/ifelse/Program.cs
@@ -8,10 +8,17 @@ namespace ifelse
         {
         int x = 9;
         int y = 3;
+        int a = 5;
+        int b = 5;
  
         if (x > y)
             { 
                 Console.WriteLine("x is greater than y");
+            }
+        
+        if (a == b) 
+            {
+            Console.WriteLine("Equal");
             }
         }
     }
