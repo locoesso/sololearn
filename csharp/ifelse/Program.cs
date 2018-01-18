c3970ab30a9fc12c17884e16f1e94f7f9009400a
Esa Hanikka <esa.hanikka@gmail.com>
2018-01-18 19:25:22 +0200

    1010110110

---
+++ csharp/ifelse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int x = 9;
+        int y = 3;
+ 
+        if (x > y)
+            { 
+                Console.WriteLine("x is greater than y");
+            }
+        }
+    }
+}
